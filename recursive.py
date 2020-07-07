cd3e0c60c2624698f6011b4b9362b3ba16a3c4d4
EdmundGoodman <egoodman3141@gmail.com>
2020-07-07 16:16:11 +0100

    Updated function calls to fit name previous name changes which were causing name errors

--- recursive.py
+++ recursive.py
@@ -5,9 +5,9 @@ def solve(b, fr=0, to=2, ot=1, depth=1):
         b.movepiece(fr,to)
 
     else:
-        solveC(b, fr, ot, to, depth+1)
+        solve(b, fr, ot, to, depth+1)
         b.movepiece(fr,to)
-        solveC(b, ot, to, fr, depth+1)
+        solve(b, ot, to, fr, depth+1)
 
 
 b = GameBoard(7)
